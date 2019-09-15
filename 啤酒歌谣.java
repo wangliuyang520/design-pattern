21d9c6f085bfb6beed0b5980043dbe6b250225f4
wangliuyang520 <1842184157@qq.com>
2019-09-15 22:46:33 +0800

    童谣

---
+++ 啤酒歌谣.java
@@ -0,0 +1,18 @@
+package Number;
+
+public class 啤酒歌谣 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+		int N;
+		for(N=99;N>0;N--)
+		{
+			if(N>1)
+				System.out.println(N+"bottles of beer on the wall,"+N+"bottes of beer.\nTake one down.\n"+N+"bottles of beer on the wall.\nPass it around");
+			else
+				System.out.println(N+" bottle of beer on the wall,"+N+"botte of beer.\nTake one down.\n"+N+"bottle of beer on the wall.\nPass it around\n No more bottles of beer on the wall");
+		}
+	}
+
+}
